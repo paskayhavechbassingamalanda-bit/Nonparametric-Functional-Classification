e5aa769fdef3f246b02b9d254d3aa1381e919a97
paskayhavechbassingamalanda-bit <paskayhavechbassingamalanda@gmail.com>
2026-08-15 03:44:08 +0000

    Update run_simulation.R

--- R/simulation/run_simulation.R
+++ R/simulation/run_simulation.R
@@ -1,1 +1,41 @@
+###############################################################
+# Generate a simulated sample
+#
+# Master's Thesis:
+# Nonparametric Classification for Spatial Functional Data
+# under Random Censoring
+#
+# Author: Paska Bassinga Malanda
+###############################################################
 
+# Load the data generation function
+source("R/data_generation.R")
+
+# ------------------------------------------------------------
+# Simulation parameters
+# ------------------------------------------------------------
+
+n1 <- 10
+n2 <- 10
+a <- 0.1
+sigma <- 0.5
+
+# ------------------------------------------------------------
+# Generate one sample
+# ------------------------------------------------------------
+
+Dn <- data2(
+  n1 = n1,
+  n2 = n2,
+  a = a,
+  sigma = sigma
+)
+
+# ------------------------------------------------------------
+# Dimensions of the simulated data
+# ------------------------------------------------------------
+
+dim(Dn)
+
+# Display the first observations
+head(Dn)
